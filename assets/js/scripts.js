c774bd7353902d67a100a2c9112e8a10b66349c4
Saulo_Calixto <brunocalisto9@gmail.com>
2022-12-10 01:57:37 -0300

    Implementando funções JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+var componenteSpan = document.getElementById("currentNumber");
+var valorAtual = 0;
+
+function incrementar(){
+    valorAtual = valorAtual + 1;
+    componenteSpan.innerHTML = valorAtual;
+}
+
+function decrementar(){
+    valorAtual = valorAtual - 1;
+    componenteSpan.innerHTML = valorAtual;
+}
