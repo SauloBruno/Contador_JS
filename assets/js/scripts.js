7ea0f9f2af32b4e55f67defbbf2c93a888fa7452
Saulo_Calixto <brunocalisto9@gmail.com>
2022-12-10 02:07:25 -0300

    condicionais

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,10 +3,18 @@ var valorAtual = 0;
 
 function incrementar(){
     valorAtual = valorAtual + 1;
+    if(valorAtual >= 0)
+    {
+        componenteSpan.style.color = "white";
+    }
     componenteSpan.innerHTML = valorAtual;
 }
 
 function decrementar(){
     valorAtual = valorAtual - 1;
+    if(valorAtual < 0)
+    {
+        componenteSpan.style.color = "red";    
+    }
     componenteSpan.innerHTML = valorAtual;
 }
